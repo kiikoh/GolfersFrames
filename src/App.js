34ae83ae02437ede151453ebef83111e4c2ed993
kiikoh <ikikomonkey@gmail.com>
2021-11-26 09:53:01 -0500

    saved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
 import FramePreviewPage from "./Pages/FramePreviewPage";
+import "./styles.css";
 
 function App() {
   return (
--- src/Pages/FramePreviewPage.jsx
+++ src/Pages/FramePreviewPage.jsx
@@ -1,20 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import courses from "../master.json"
 import Frame from '../Components/Frame'
+import Form from '../Components/Form'
+import Gallery from '../Components/Gallery'
 
 const FramePreviewPage = () => {
 
     const [course, setCourse] = useState(courses[0])
+    const [hole, setHole] = useState(course.assets.holes[0])
+    const [form, setForm] = useState({});
+
+    useEffect(() => {
+        setHole(course.assets.holes[+form.holeIndex]) // keep the hole obj updated with the index of it
+    }, [form.holeIndex, course.assets.holes])
 
     return (
         <div>
             <select onChange={(e) => setCourse(courses[e.target.value])}>
-                {courses.map((course, key) => {
-                    return <option value={key} key={key}>{course.courseName}</option>
-                })}
+                {courses.map((course, key) => 
+                    <option value={key} key={key}>{course.courseName}</option>
+                )}
             </select>
-            <Frame course={course}/>
+            <div id="mainContainer">
+                <div id="frameForm">
+                    <Frame course={course} hole={hole} form={form} />
+                    <Form course={course} setForm={setForm} />
+                </div>
+                <Gallery course={course}/>
+            </div>
         </div>
+    
     )
 }
 
