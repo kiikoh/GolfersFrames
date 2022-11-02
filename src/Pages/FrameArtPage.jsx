22101ce172e1c99f1cc62ab3fb862c53cd59e6b6
kiikoh <ikikomonkey@gmail.com>
2022-11-02 10:40:36 -0400

    change frame colors, and the way they are chosen, frame-large

--- src/Pages/FrameArtPage.jsx
+++ src/Pages/FrameArtPage.jsx
@@ -1,15 +1,15 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import Frame from '../Components/Frame'
 import Gallery from '../Components/Gallery'
 import { FormControl, Grid, InputLabel, MenuItem, Select, Stack, Typography } from '@mui/material'
 import { Controller, useForm } from "react-hook-form"
 import { useTheme } from '@mui/material'
-import {useIdle} from "react-use"
+import { useIdle } from "react-use"
 
 const IDLE_TIME = 30;
 const SLIDE_TIME = 4;
 
-const FrameArtPage = ({course, children}) => {
+const FrameArtPage = ({ course, children }) => {
 
     const theme = useTheme()
     const isIdle = useIdle(IDLE_TIME * 1000, true);
@@ -39,23 +39,23 @@ const FrameArtPage = ({course, children}) => {
     return (
         <Grid container spacing={1}>
             <Grid item xs={12} md={7}>
-                <Stack style={{marginTop: "2vh"}}>
-                    <Typography variant="h4" sx={{textAlign: "center", color: theme.palette.primary.main}}>Select your favorite hole!</Typography>
+                <Stack style={{ marginTop: "2vh" }}>
+                    <Typography variant="h4" sx={{ textAlign: "center", color: theme.palette.primary.main }}>Select your favorite hole!</Typography>
                     <Frame course={course} hole={course.assets.holes[+watch("holeIndex")]} form={watch()} />
-                    <Typography variant="h6" sx={{textAlign: "center", color: theme.palette.primary.main}}>Available Sizes: Small - 24"x14" (20"x10" print) | Medium - 28"x16" (24"x12")|  Large - 34"x19" (30"x15") | Extra Large - 44"x24" (40"x20")</Typography>
+                    <Typography variant="h6" sx={{ textAlign: "center", color: theme.palette.primary.main }}>Available Sizes: Small - 24"x14" (20"x10" print) | Medium - 28"x16" (24"x12")|  Large - 34"x19" (30"x15") | Extra Large - 44"x24" (40"x20")</Typography>
                     <Grid marginTop={2} container>
                         <Grid item xs={4}></Grid>
                         <Grid item xs={4}>
                             <Controller
                                 name="color"
                                 control={control}
-                                render={({field}) => <FormControl fullWidth>
-                                        <InputLabel id="color-label">Frame Color</InputLabel>
-                                        <Select size="small" {...field} label="Frame Color" labelId="color-label">
-                                            <MenuItem value="brown">Brown Mahogany</MenuItem>
-                                            <MenuItem value="red">Red Mahogany</MenuItem>
-                                        </Select>
-                                    </FormControl>
+                                render={({ field }) => <FormControl fullWidth>
+                                    <InputLabel id="color-label">Frame Color</InputLabel>
+                                    <Select size="small" {...field} label="Frame Color" labelId="color-label">
+                                        <MenuItem value="brown">Small / Medium</MenuItem>
+                                        <MenuItem value="red">Large / Extra Large</MenuItem>
+                                    </Select>
+                                </FormControl>
                                 }
                             />
                         </Grid>
@@ -65,8 +65,8 @@ const FrameArtPage = ({course, children}) => {
                 </Stack>
             </Grid>
             <Grid item xs={12} md={5}>
-                <Typography sx={{fontWeight: "bold"}} variant="h4" my={2} align="center" color={theme.palette.primary.main}>{course.courseName}</Typography>
-                <Gallery course={course} form={watch()} setValue={setValue}/>
+                <Typography sx={{ fontWeight: "bold" }} variant="h4" my={2} align="center" color={theme.palette.primary.main}>{course.courseName}</Typography>
+                <Gallery course={course} form={watch()} setValue={setValue} />
             </Grid>
         </Grid>
     )
